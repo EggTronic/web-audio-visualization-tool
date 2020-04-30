3079443992188553e6412dc1abb657e0455c91c2
YANG XU <yaxu2@student.unimelb.edu.au>
2020-04-30 19:15:20 +1000

    [add] loading status

--- defaultRenderHooks.js
+++ defaultRenderHooks.js
@@ -169,7 +169,6 @@ export const renderPlayButton = (avCtx) => {
       e.stopPropagation();
       if (!avCtx.isPlaying) {
         if (avCtx.isLoading) {
-          console.log(1);
           return;
         }
         return (avCtx.audio.paused) ? avCtx.playSound() : avCtx.loadSound();
