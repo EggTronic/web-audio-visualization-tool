484501aa46152b8d804b35de3d5c08a25422f1ea
YANG XU <yaxu2@student.unimelb.edu.au>
2020-05-14 22:35:47 +1000

    update element class

--- Element.js
+++ Element.js
@@ -1,28 +1,39 @@
 class Element {
-    constructor(x, y, type, others){
-        this.x = x
-        this.y = y
-        this.type = type
-        this.others = others
+    constructor(x, y, type, theme, audio, frequencyData, onClick, onKeys){
+        this.x = x;
+        this.y = y;
+        this.type = type;
+        this.theme = theme;
+        this.audio = audio;
+        this.frequencyData = frequencyData;
+        this.handleClick = onClick;
+        this.handleKeys = onKeys;
+    }
+
+    _init() {
+        // check event type
+        // iterate events to bind
+        // return functions to un-subscribe events
     }
 }
 
 export class CanvasElement extends Element {
-    constructor(x, y, type, others) {
-        super(x, y, type, others)
+    constructor(x, y, type, theme, audio, frequencyData, onClick, onKeys, canvas) {
+        super(x, y, type, theme, audio, frequencyData, onClick, onKeys);
+        this.canvas = canvas;
     }
 
-    render(avCtx){
+    render(){
         
     }
 }
 
 export class ThreeElement extends Element {
     constructor(x, y, type, others) {
-        super(x, y, type, others)
+        super(x, y, type, others);
     }
 
-    render(avCtx){
+    render(canvas, audio, frequencyData){
         
     }
 }
